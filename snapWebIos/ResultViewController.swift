18d6d6c17c5bf03f1804ac11f3a01f9ae9e01521
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-07-05 13:49:53 +0700

    create ResultViewController

---
+++ snapWebIos/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  snapWebIos
+//
+//  Created by Muhammad Masykur on 05/07/24.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
